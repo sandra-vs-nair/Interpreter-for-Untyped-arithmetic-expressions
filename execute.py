013431a362a4fa757008574716ca82c7a73c435c
sandra <sandravsnair@gmail.com>
2021-04-09 21:04:13 +0530

    Adding and-or-not stuck term logic and solved 0-int issue

--- execute.py
+++ execute.py
@@ -10,23 +10,16 @@ flag = 0
 def flatten(L):
     
     if len(L) == 1:
-        
-            if type(L[0]) == list:
-                
-                    result = flatten(L[0])
-                    
-            else:
-                
-                    result = L
-                
+        if type(L[0]) == list:
+            result = flatten(L[0])
+        else:
+            result = L
     elif type(L[0]) == list:
-        
-            result = flatten(L[0]) + flatten(L[1:])
-            
+        result = flatten(L[1:])
+        result = flatten(L[0]) + result
     else:
-        
-            result = [L[0]] + flatten(L[1:])
-        
+        result = flatten(L[1:])
+        result = [L[0]] + result
     return result
 
 def listToString(s):
@@ -44,7 +37,7 @@ def eval_term(t):
     elif t == 'false' or t == False:
         return 'false'
     elif t == 'zero':
-        return 0
+        return '0'
 
     elif t[0] == 'succ' and t[1] == 'zero':
         return 'succ 0'
@@ -80,8 +73,8 @@ def eval_term(t):
         t1 = eval_term(t[1])
         t2 = eval_term(t[2])
         t3 = eval_term(t[3])
-        v = ['true', 'false']
-        if t1 in v and t2 in v and t2 in v:
+        b = ['true', 'false']
+        if t1 in b and t2 in b and t2 in b:
             t1 = True if t1 == "true" else False
             t2 = eval_term(t[2])
             t3 = eval_term(t[3])
@@ -89,18 +82,38 @@ def eval_term(t):
             t3 = True if t3 == "true" else False
             return (t1 and (t2 or(not t3))) 
         else:
-            two = ""
-            three = ""
-            if(t[2] == "true" or t[2] == "false"):
-                two = t[2]
-            else:
-                two = listToString(t[2])
-            if(t[3] == "true" or t[3] == "false"):
-                three = t[3]
-            else:
-                three = listToString(t[3])
+            ch = 0
 
-            return (t[0]+" "+t1+" "+"or"+" "+two+" "+"not"+" "+three)
+            if t3 in b:
+                t3 = True if t3 == "true" else False
+                ch = 1
+                v = not t3
+                res = "true" if v == True else "false"
+            else:
+                res = "not"+" ( "+t3+" ) "
+
+            if t2 in b:
+                if ch == 1:
+                    t2 = True if t2 == "true" else False
+                    v = t2 or v
+                    res = "true" if v == True else "false"
+                else:
+                    res = "or"+" ( "+t2+" ) "+res
+            else:
+                ch = 0
+                res = "or"+" ( "+t2+" ) "+res
+
+            if t1 in b:
+                if ch == 1:
+                    t1 = True if t1 == "true" else False
+                    v = t1 and v
+                    res = "true" if v == True else "false"
+                else:
+                    res = "and"+" ( "+t1+" ) "+res
+            else:
+                res = "and"+" ( "+t1+" ) "+res
+
+            return res
 
     elif t[0] == 'if':
         val = eval_term(t[1])
@@ -113,13 +126,18 @@ def eval_term(t):
             three = ""
             if(t[2] == "true" or t[2] == "false"):
                 two = t[2]
+            elif(t[2] == "zero"):
+                two = "0"
             else:
                 two = listToString(t[2])
+
             if(t[3] == "true" or t[3] == "false"):
                 three = t[3]
+            elif(t[3] == "zero"):
+                three = "0"
             else:
                 three = listToString(t[3])
             
-            return (t[0]+" "+val+" "+"then"+" "+two+" "+"else"+" "+three)
+            return (t[0]+" ( "+val+" ) "+"then"+" ( "+two+" ) "+"else"+" ( "+three+" ) ")
     else:
         return listToString(t)
